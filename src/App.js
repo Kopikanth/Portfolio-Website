6f5501d2f178af90db851d5b4807b5f0495921bb
Kopikanth <118206393+Kopikanth@users.noreply.github.com>
2025-08-21 20:33:49 +0530

    created skills section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Intro from "./Components/Intro/Intro";
 import NavBar from "./Components/NavBar/NavBar";
+import Skills from "./Components/Skills/Skills";
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Intro/>
+      <Skills/>
     </div>
   );
 }
--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,11 +1,15 @@
-import './Skills.css'
+import './Skills.css';
+import uiDesign from '../../assets/ui-design.png';
+import websiteDesign from '../../assets/website-design.png';
+import appDesign from '../../assets/app-design.png';
 
 const Skills = () => {
 
     return (
-        <div>
-
-        </div>
+        <section className="skills">
+            <span className="skillsTitle">What I do</span>
+            <p className="skillsDescription">I am a skilled and passionate web designer with experience in creating visually appealing and user-friendly websites. I have a strong understanding of design and a keen eye for detail. I am proficient in html,css and javascript, as well as design software such as Adobe Photoshop and Illustrator.</p>
+        </section>
     );
 };
 
